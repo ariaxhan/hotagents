177deb0c80674849c76000141ab2f772607de6eb
Manik Sethi <mksethi@ucdavis.edu>
2024-05-18 19:57:18 -0700

    adding GUI

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 800,
+    width: 900,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
